c638fdb03e724f1d05a3215b7de9d054222c0cdf
Blake Gallay <blakegallay@google.com>
2022-12-12 17:11:45 -0800

    Initial commit

--- infer/src/labs/ResourceLeakDomain.mli
+++ infer/src/labs/ResourceLeakDomain.mli
@@ -11,4 +11,20 @@ include AbstractDomain.S
 
 val initial : t
 
-type summary = t
+val acquire_resource : AccessPath.t -> t -> t
+
+val release_resource : AccessPath.t -> t -> t
+
+val assign : AccessPath.t -> AccessPath.t -> t -> t
+
+val has_leak : t -> bool
+
+val record_return_resource : t -> t
+
+type summary
+
+val make_summary : t -> summary
+
+val apply_summary : t -> summary -> AccessPath.t -> t
+
+val pp_summary : Format.formatter -> summary -> unit
